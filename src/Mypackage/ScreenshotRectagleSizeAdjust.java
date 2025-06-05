b768944864904908671a2d65b9b86fbad7d3481c
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-05 11:39:00 +0530

    screenshot robot

---
+++ src/Mypackage/ScreenshotRectagleSizeAdjust.java
@@ -0,0 +1,55 @@
+package Mypackage;
+
+import java.awt.AWTException;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ScreenshotRectagleSizeAdjust {
+	public static void main(String[] args) throws AWTException, IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://gspedia.com/projects/green");
+		driver.manage().window().maximize();
+		
+//		Rectangle captureRect = new Rectangle(100, 100, 500, 300);
+//		Thread.sleep(5000);
+//        // Capture the screen area defined by the rectangle
+//        BufferedImage image = new Robot().createScreenCapture(captureRect);
+//        File path = new File("C:\\Screenshot\\PartialScreenshot.png");
+//        // Save the screenshot to a specific location
+//        ImageIO.write(image, "png", path);
+//
+//        System.out.println("Screenshot saved successfully!");
+		
+		// Wait for the page to load
+        Thread.sleep(5000);
+
+        // Take a screenshot of the entire page
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        // Read the screenshot as a BufferedImage
+        BufferedImage fullImg = ImageIO.read(screenshot);
+
+        // Crop the screenshot to a rectangle (x, y, width, height)
+        BufferedImage croppedImg = fullImg.getSubimage(100, 100, 500, 300);
+
+        // Save the cropped screenshot
+        File path = new File("C:\\Screenshot\\PartialScreenshot.png");
+        ImageIO.write(croppedImg, "png", path);
+
+        System.out.println("Cropped screenshot saved successfully!");
+
+        // Close the browser
+        driver.quit();
+
+	}
+}
