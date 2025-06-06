f09efc50fde80c20c8fb8efb9db40bd6ecc70f21
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-06 11:26:14 +0530

    fileupload RBOT

--- src/Mypackage/FileUploadRobot.java
+++ src/Mypackage/FileUploadRobot.java
@@ -1,5 +1,55 @@
 package Mypackage;
 
-public class FileUploadRobot {
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FileUploadRobot {
+	public static void main(String[] args) throws AWTException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.guru99.com/test/upload/");
+		driver.manage().window().maximize();
+		 
+    	WebElement uploadElement = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[1]/form/div[2]/div[1]/div/div"));
+    	uploadElement.click();
+ 
+    	Robot robot = new Robot();
+
+        String filePath = "C:\\Users\\HP\\Desktop\\abc.html";
+       
+        
+        robot.delay(1000); 
+//        for (char c : filePath.toCharArray()) {
+//            robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(c));
+//            robot.keyRelease(KeyEvent.getExtendedKeyCodeForChar(c));
+//        }
+        
+     // Set the file path to clipboard
+        StringSelection selection = new StringSelection(filePath);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(selection,null);
+
+        //  Ctrl+V (paste) 
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+
+        //  Enter 
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+        
+        driver.findElement(By.id("terms")).click();
+
+        driver.findElement(By.name("send")).click();
+
+	}
 }
