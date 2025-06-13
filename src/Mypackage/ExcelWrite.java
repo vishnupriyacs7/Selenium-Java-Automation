9f70db464e18ace017d7b1b6fbbe5f33aa68f8eb
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-13 12:21:19 +0530

    excel write

--- src/Mypackage/ExcelWrite.java
+++ src/Mypackage/ExcelWrite.java
@@ -4,6 +4,11 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 
 
 public class ExcelWrite {
