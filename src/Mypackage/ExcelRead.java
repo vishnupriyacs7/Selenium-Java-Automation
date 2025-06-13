cea79f6a554e090e75bb9e9dca3943bf684b8420
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-13 12:09:43 +0530

    excel read

--- src/Mypackage/ExcelRead.java
+++ src/Mypackage/ExcelRead.java
@@ -1,5 +1,29 @@
 package Mypackage;
 
-public class ExcelRead {
+import java.io.FileInputStream;
+import java.io.IOException;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+
+public class ExcelRead {
+	public static void main(String[] args) throws IOException {
+		
+		FileInputStream fis = new FileInputStream("C:\\Users\\HP\\Desktop\\halifax.xlsx");
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		XSSFSheet sheet = workbook.getSheetAt(0);
+		
+		XSSFRow row = sheet.getRow(0);
+		XSSFCell cell = row.getCell(0);
+		System.out.println(cell);
+
+		System.out.println(sheet.getRow(0).getCell(1));
+	
+
+
+}
 }
